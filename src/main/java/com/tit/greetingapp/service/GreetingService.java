823abc062ec13cb8863a2c547c5d1ad4097436cb
Niraj Kumar <nirajgupta7481@gmail.com>
2025-02-22 21:49:29 +0530

    [Niraj] Add . uc4 feature added

--- src/main/java/com/tit/greetingapp/service/GreetingService.java
+++ src/main/java/com/tit/greetingapp/service/GreetingService.java
@@ -1,12 +1,20 @@
 package com.tit.greetingapp.service;
 
+import com.tit.greetingapp.model.Greeting;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
 
 @Service
 public class GreetingService {
     public String getGreetingMessage() {
         return "Hello World";
     }
+
+    private final Map<Long, Greeting> greetings = new HashMap<>();
+    private final AtomicLong idCounter = new AtomicLong();
 
     // UC3: Personalized Greeting (New Feature)
     public String getPersonalizedGreeting(String firstName, String lastName) {
@@ -20,5 +28,17 @@ public class GreetingService {
             return getGreetingMessage();  // Reusing UC2 method for default}
         }
     }
+
+    // UC4 - Save a Greeting message
+    public Greeting saveGreeting(String message) {
+        long id = idCounter.incrementAndGet();
+        Greeting greeting = new Greeting(id, message);
+        greetings.put(id, greeting);
+        return greeting;
+    }
+
+    // UC4 - Retrieve a Greeting by ID
+    public Greeting getGreetingById(Long id) {
+        return greetings.get(id);}
 }
 
